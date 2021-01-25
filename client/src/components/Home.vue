d8365cdcd9fb25a091aacf75b751790fb4d515fc
Esme-Marie <esmemarie.sultan@gmail.com>
2021-01-26 07:36:11 +0800

    JIWA-15 Create navigation with page routers

---
+++ client/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <div class="home">
+      <h1>Home</h1>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+
+    data: () => ({
+      msg: 'Welcome to Your Vue.js App'
+    }),
+  }
+</script>
---
+++ client/src/components/Member.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <div class="member">
+            <h1>Member</h1>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "member",
+
+    data: () => ({
+        title: "Member",
+    }),
+};
+</script>
