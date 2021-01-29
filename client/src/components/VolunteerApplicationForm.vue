cd223d2a4c8e23a438ad84e81e72cc73b3632493
Jannah <jannahworks@gmail.com>
2021-01-29 16:02:15 +0800

    JIWA-34 add forms with fields (volunteers)

--- client/src/components/VolunteerApplicationForm.vue
+++ client/src/components/VolunteerApplicationForm.vue
@@ -2,48 +2,53 @@
     <v-card>
         <v-card-text>
             <v-container>
-                <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field label="First name*" required></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-text-field label="Last name*" hint="example of persistent helper text" persistent-hint
-                            required></v-text-field>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-text-field label="Email*" required></v-text-field>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-text-field label="Contact Number*" required></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-text-field label="Address 1*" required></v-text-field>
-                    </v-col>   
-                    <v-col cols="12">
-                        <v-text-field label="Address 2"></v-text-field>
-                    </v-col>  
-                    <v-col cols="4">
-                        <v-text-field label="State" required></v-text-field>
-                    </v-col>                    
-                    <v-col cols="4">
-                        <v-text-field label="State" required></v-text-field>                      
-                    </v-col>    
-                    <v-col cols="4">
-                        <v-text-field label="Country" required></v-text-field>
-                    </v-col>                                                      
-                    <!-- <v-col cols="12" sm="6">
+                <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-row>
+                        <v-col cols="6">
+                            <v-text-field label="First name*" v-model="formValues.firstname" :counter="10" :rules="nameRules"
+                                required></v-text-field>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field label="Last name*" v-model="formValues.lastname" required></v-text-field>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field label="Email*" v-model="formValues.email" required></v-text-field>
+                        </v-col>
+                        <v-col cols="6">
+                            <v-text-field label="Contact Number*" v-model="formValues.contactnum" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Address 1*" v-model="formValues.address1" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Address 2" v-model="formValues.address2"></v-text-field>
+                        </v-col>
+                        <v-col cols="4">
+                            <v-text-field label="Postcode" v-model="formValues.postcode" required></v-text-field>
+                        </v-col>
+                        <v-col cols="4">
+                            <v-text-field label="State" v-model="formValues.state" required></v-text-field>
+                        </v-col>
+                        <v-col cols="4">
+                            <v-text-field label="Country" v-model="formValues.country" required></v-text-field>
+                        </v-col>
+                        <!-- <v-col cols="12" sm="6">
                         <v-autocomplete
                             :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
                             label="Interests" multiple></v-autocomplete>
                     </v-col> -->
-                </v-row>
+                    </v-row>
+                </v-form>
             </v-container>
             <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="$emit('closeForm')">
+            <v-btn color="blue darken-1" text @click="handleClose">
                 Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="$refs.form.reset()">
+                Reset
             </v-btn>
             <v-btn color="blue darken-1" text @click="handleSubmit">
                 Submit
@@ -56,13 +61,37 @@
     export default {
         name: 'VolunteerApplicationForm',
         props: {},
+        data() {
+            return {
+                valid: true,
+                nameRules: [
+                    v => !!v || 'Name is required',
+                    v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+                ],
+                formValues: {
+                    firstname: "",
+                    lastname: "",
+                    email: "",
+                    contactnum: "",
+                    address1: "",
+                    address2: "",
+                    state: "",
+                    country: "",
+                    postcode: "",
+                    dateapp: null
+                }
+            }
+        },
 
         methods: {
-            handleDialog() {
-                this.$emit('handleDialog');
-            },
             handleSubmit() {
-                
+                this.$refs.form.validate();
+                this.$refs.form.reset();
+                this.$emit('closeForm');
+            },
+            handleClose() {
+                this.$refs.form.reset();
+                this.$emit('closeForm');
             }
         }
 
