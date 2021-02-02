eeaafc817dc075d634ea409b3d4edc912b4d9c6d
njas <jannahworks@gmail.com>
2021-02-02 16:11:48 +0800

    merge back to br-staging

---
+++ client/src/components/Member copy.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <div class="member">
+            <h1>Member</h1>
+            <create-event-form :organizerId="userId" />
+        </div>
+        <volunteer-list :eventId="selectedEvent" />
+    </v-container>
+</template>
+
+<script>
+import CreateEventForm from './CreateEventForm';
+import VolunteerList from './VolunteerList';
+
+export default {
+    components: { CreateEventForm, VolunteerList },
+    name: "member",
+
+    data: () => ({
+        title: "Member",
+        selectedEvent: 1,
+        userId: 3
+    }),
+};
+</script>
