9031f351aff4d435957394d21aec18a325ec70c9
njas <jannahworks@gmail.com>
2021-02-02 15:35:09 +0800

    JIWA-70 add fields, change field name

--- client/src/components/Member.vue
+++ client/src/components/Member.vue
@@ -2,20 +2,24 @@
     <v-container>
         <div class="member">
             <h1>Member</h1>
-            <create-event-form />
+            <create-event-form :organizerId="userId" />
         </div>
+        <volunteer-list :eventId="selectedEvent" />
     </v-container>
 </template>
 
 <script>
 import CreateEventForm from './CreateEventForm';
+import VolunteerList from './VolunteerList';
 
 export default {
-    components: { CreateEventForm },
+    components: { CreateEventForm, VolunteerList },
     name: "member",
 
     data: () => ({
         title: "Member",
+        selectedEvent: 1,
+        userId: 3
     }),
 };
 </script>
