ebc687675d3f1a1c12c7c616c4b8828c36cfbf20
njas <jannahworks@gmail.com>
2021-02-14 07:17:08 +0800

    update UI

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let createError = require('http-errors');
 let express = require('express');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-//let cors = require("cors");
+let cors = require("cors");
 
 // let indexRouter = require('./routes/index');
 let apiRouter = require('./routes/api');
@@ -10,11 +10,14 @@ let userRouter = require('./routes/users');
 
 let app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Serve static files from the React frontend app
+//app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("/", function(req, res, next) {
   res.send("Access the API at path /api");
@@ -23,20 +26,27 @@ app.get("/", function(req, res, next) {
 app.use('/api', apiRouter)
 app.use('/api/auth', userRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const port = 5000;
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Anything that doesn't match the above, send back index.html
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send('error');
+// });
 
 module.exports = app;
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,14 +1,30 @@
+const path = require('path');
 module.exports = {
-  "devServer": {
-    "proxy": {
-      "^/api": {
-        "target": "http://localhost:5000",
-        "changeOrigin": true
+  outputDir: path.resolve(__dirname, '../client/dist'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'
       }
     }
   },
   "transpileDependencies": [
-    "vuetify",
-    "vue-tel-input-vuetify"
+  "vuetify",
+  "vue-tel-input-vuetify"
   ]
 }
+
+// module.exports = {
+//   "devServer": {
+//     "proxy": {
+//       "^/api": {
+//         "target": "http://localhost:5000",
+//         "changeOrigin": true
+//       }
+//     }
+//   },
+//   "transpileDependencies": [
+//     "vuetify",
+//     "vue-tel-input-vuetify"
+//   ]
+// }
