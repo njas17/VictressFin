692ec790918e2cd9aecbc5f2ce773bbbcaa46348
njas <jannahworks@gmail.com>
2021-02-14 08:20:08 +0800

    anything

--- app.js
+++ app.js
@@ -33,20 +33,20 @@ const port = 5000;
 //   res.sendFile(path.join(__dirname + "/client/dist/index.html"));
 // });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.send('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
 
 module.exports = app;
