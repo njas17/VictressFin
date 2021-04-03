cdd2fb0e729a4546b892fe43c7716af83175e67c
njas <jannahworks@gmail.com>
2021-04-03 09:28:23 +0800

    fix file

--- client/src/router.js
+++ client/src/router.js
@@ -8,7 +8,7 @@ import AuthPage from './components/AuthPage.vue';
 import SignOut from './components/SignOut.vue';
 import NotFound from './components/NotFound.vue';
 import store from './store.js';
-import genderLens from './components/genderLens.vue';
+import genderLens from './components/GenderLens.vue';
 import Equity from './components/Equity.vue';
 import Education from './components/Education.vue';
 // import Equity from '.components/Equity.vue';
